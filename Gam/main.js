94678ff75f178e34951deff34fbb8717744dcd78
Romas Cebis <edsromas@gmail.com>
2013-11-04 22:18:01 +0200

    Changed some Art and added to the sprite repository

--- Gam/main.js
+++ Gam/main.js
@@ -27,14 +27,14 @@ $().ready(function () {
     mainCtx.canvas.addEventListener("click", function () {
         var hoveredTile = tileRepo.getHoveredTile();
         if (hoveredTile != null) {     
-            var spriteData = spriteRepo.get("canon");
+            var spriteData = spriteRepo.get("harvester");
             hoveredTile.object = new Sprite(spriteData.frameWidth, spriteData.frameSequence, spriteData.speed, spriteData.img);
         }
     });
     mainCtx.canvas.oncontextmenu = function () {
         var hoveredTile = tileRepo.getHoveredTile();
         if (hoveredTile != null) {
-            var spriteData = spriteRepo.get("harvester");
+            var spriteData = spriteRepo.get("shield");
             hoveredTile.object = new Sprite(spriteData.frameWidth, spriteData.frameSequence, spriteData.speed, spriteData.img);
         }
         return false;
@@ -42,8 +42,10 @@ $().ready(function () {
   
     tileRepo.createTiles(worldParams);
 
-    spriteRepo.add("harvester", "img/harvester.png", 50, 26, 50, [0], 0);
-    spriteRepo.add("canon", "img/canon.png" , 294, 40, 50, [0,1,2,3,4,5,6,6,6,6,6,6,6,6,5,4,3,2,1,0], 50);
+    spriteRepo.add("harvester", "img/harvester.png", 450, 36, 50, [0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,1,1,2,1,1,2,1,2,1,2,3,4,5,6,7,8,1,1,1,1,1,0,0,0,0], 100);
+    spriteRepo.add("canon", "img/canon.png", 294, 40, 50, [0, 1, 2, 3, 4, 5, 6, 6, 6, 6, 6, 6, 6, 6, 5, 4, 3, 2, 1, 0], 50);
+    spriteRepo.add("shield", "img/shield.png", 150, 100, 150, [0], 0);
+
 
     //start the main loop   
     animFrame(animationLoop);
